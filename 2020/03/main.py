c4d859134c065912207d26e4af18509a68aa6e35
Micah Wood <mxw7265@rit.edu>
2021-11-27 09:59:41 -0700

    Added 2020 Day 3 Solution

---
+++ 2020/03/main.py
@@ -0,0 +1,54 @@
+FILENAME = "input.txt"
+OPEN = '.'
+TREE = '#'
+
+def get_input() -> list[str]:
+    """ Read the file and generate a grid map (list of strings)
+    """
+    strMap = list()
+
+    with open(FILENAME) as f:
+        for line in f.readlines():
+           strMap.append(line.strip())
+
+    return strMap
+
+def count_collision(strMap: list[str], right: int, down: int) -> int:
+    """ Read the map and count how many tree would be encountered if someone start from the top left corner    
+    """
+    mapWidth = len(strMap[0]) # All lines are assumed to have same width
+    xCoord, yCoord = right % mapWidth, down
+    count = 0
+
+    while yCoord < len(strMap):
+        if strMap[yCoord][xCoord] == TREE:
+            count += 1
+
+        xCoord = (xCoord + right) % mapWidth
+        yCoord += down
+
+    return count
+
+def part_1():
+    strMap = get_input()
+
+    answer = count_collision(strMap, 3, 1)
+
+    print(f"{answer} trees will be encountered")
+
+def part_2():
+    strMap = get_input()
+    
+    a1 = count_collision(strMap, 1, 1)
+    a2 = count_collision(strMap, 3, 1)
+    a3 = count_collision(strMap, 5, 1)
+    a4 = count_collision(strMap, 7, 1)
+    a5 = count_collision(strMap, 1, 2)
+
+    answer = a1 * a2 * a3 * a4 * a5
+
+    print(f"The result of multiplication of the number of trees encountered on each of the listed slopes is {answer}")
+
+if __name__ == "__main__":
+    part_1()
+    part_2()
--- README.md
+++ README.md
@@ -56,7 +56,7 @@ I will start and catch up after 2021 event ends!
 <br />
 
 # 2020
-|[Day 1](2020/01/main.py)|[Day 2](2020/02/main.py)|[Day 3]|[Day 4](2020/04/main.py)|[Day 5](2020/05/main.py)|
+|[Day 1](2020/01/main.py)|[Day 2](2020/02/main.py)|[Day 3](2020/03/main.py)|[Day 4](2020/04/main.py)|[Day 5](2020/05/main.py)|
 | :---: | :---: | :---: | :---: | :---: |
 |[**Day 6**](2020/06/main.py)|[**Day 7**](2020/07/main.py)|[**Day 8**](2020/08/main.py)|[**Day 9**](2020/09/main.py)|[**Day 10**](2020/10/main.py)|
 |[**Day 11**](2020/11/)|[**Day 12**](2020/12/main.py)|[**Day 13**](2020/13/main.py)|[**Day 14**](2020/14/main.py)|[**Day 15**](2020/15/main.py)|
