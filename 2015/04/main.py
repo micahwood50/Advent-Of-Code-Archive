04decf7d342a75c8adbbc462c55e6fc3b4304b34
Micah Wood <mxw7265@rit.edu>
2021-12-03 13:22:41 -0700

    Added 2015 Day 4 Solution

---
+++ 2015/04/main.py
@@ -0,0 +1,34 @@
+from hashlib import md5
+
+FILENAME = "input.txt"
+
+def get_input() -> str:
+    input = list()
+
+    with open(FILENAME) as file:
+        input = file.readline().strip()
+
+    return input
+
+def part_1():
+    input = get_input()
+    secret_key = 0
+
+    while not md5(f"{input}{secret_key}".encode()).hexdigest().startswith(5 * '0'):
+        secret_key += 1
+
+    print(f"The secret key is {secret_key}")
+
+def part_2():
+    input = get_input()
+    secret_key = 0
+
+    while not md5(f"{input}{secret_key}".encode()).hexdigest().startswith(6 * '0'):
+        secret_key += 1
+
+    print(f"The secret key is {secret_key}")
+
+if __name__ == "__main__":
+    # part_1()
+    # part_2()
+    pass
